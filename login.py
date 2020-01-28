5e72f7fb3539ae35a65fe05f554b13eeb0c48973
BoostedMercy <mhoyle2011@hotmail.co.uk>
2020-01-28 16:06:22 +0000

    Added Login + Started Main

---
+++ login.py
@@ -0,0 +1,7 @@
+import tkinter
+
+# Nathan working on login system :)
+
+def mainLogin():
+    access = bool
+    return access  # If access is true, run the program, else dont
--- main.py
+++ main.py
@@ -1,2 +1,8 @@
-import request
+import request, login
 
+LoginAccess = login.mainLogin()
+
+if LoginAccess:
+    print("Run the program")
+else:
+    print("Access denied etc.")
